83506eee51f1e84da99eef154f9ea58b84ef1491
Rafi Guzman 89 <78271445+Rafidavid89@users.noreply.github.com>
2023-05-06 22:36:59 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include "layouts/header.php"; ?>
+<div class="container">
+
+<center><h2 style="color:black; margin-top:20%">Bienvenido al chat, entra y connoce personas</h2></center>
+<center><a href="http://localhost/web%20social/chat/login.php"><img src="images/rafi.png" width="180" height="60"></a></center>
+</div>
+
+</body>
+</html>
